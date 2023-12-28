e6979758db78e0b0ec3e82414478519659e13728
Maria Ramos <maria.m.ramos@tecnico.ulisboa.pt>
2023-12-28 01:23:14 +0000

    api.c

--- proj_23-24-p2_base/client/api.c
+++ proj_23-24-p2_base/client/api.c
@@ -1,102 +1,162 @@
-#include "api.h"
 #include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
-static int session_id = -1;  // Variable to store the active session ID
-static int req_fd, resp_fd, server_pipe;  // Declare these as global variables
+#define S 10
+#define MAX_PATH 256
 
-/*responsible for setting up the EMS client by creating pipes and connecting to the server. */
-int ems_setup(char const* req_pipe_path, char const* resp_pipe_path, char const* server_pipe_path) {
-  // Create request pipe (write-end)
-  int req_pipe = mkfifo(req_pipe_path, 0666);
-  if (req_pipe == -1) {
-    perror("Error creating request pipe");
+typedef struct {
+  int session_id;
+  char req_pipe_path[MAX_PATH];
+  char resp_pipe_path[MAX_PATH];
+} Session;
+
+Session sessions[S];
+int active_sessions = 0;
+
+int ems_setup(char const *req_pipe_path, char const *resp_pipe_path, char const *server_pipe_path) {
+  // Create request and response pipes
+  mkfifo(req_pipe_path, 0666);
+  mkfifo(resp_pipe_path, 0666);
+
+  // Connect to server pipe
+  int server_fd = open(server_pipe_path, O_WRONLY);
+  if (server_fd < 0) {
     return 1;
   }
 
-  // Create response pipe (read-end)
-  int resp_pipe = mkfifo(resp_pipe_path, 0666);
-  if (resp_pipe == -1) {
-    perror("Error creating response pipe");
-    return 1;
-  }
+  // Send session start request
+  write(server_fd, req_pipe_path, strlen(req_pipe_path) + 1);
+  write(server_fd, resp_pipe_path, strlen(resp_pipe_path) + 1);
 
-  // Open server pipe for writing (send requests to the server)
-  int server_pipe = open(server_pipe_path, O_WRONLY);
-  if (server_pipe == -1) {
-    perror("Error opening server pipe for writing");
-    return 1;
-  }
-
-  // Open request pipe for writing (send requests to the server)
-  int req_fd = open(req_pipe_path, O_WRONLY);
-  if (req_fd == -1) {
-    perror("Error opening request pipe for writing");
-    return 1;
-  }
-
-  // Open response pipe for reading (receive responses from the server)
+  // Store session_id in sessions array
   int resp_fd = open(resp_pipe_path, O_RDONLY);
-  if (resp_fd == -1) {
-    perror("Error opening response pipe for reading");
+  if (resp_fd < 0) {
     return 1;
   }
+  read(resp_fd, &sessions[active_sessions].session_id, sizeof(int));
+  strcpy(sessions[active_sessions].req_pipe_path, req_pipe_path);
+  strcpy(sessions[active_sessions].resp_pipe_path, resp_pipe_path);
+  active_sessions++;
 
-  // Send session initiation request to the server
-  if (write(server_pipe, req_pipe_path, sizeof(req_pipe_path)) == -1) {
-    perror("Error sending session initiation request to the server");
-    return 1;
-  }
+  close(server_fd);
+  close(resp_fd);
 
-  if (write(server_pipe, resp_pipe_path, sizeof(resp_pipe_path)) == -1) {
-    perror("Error sending session initiation request to the server");
-    return 1;
-  }
-
-  // Receive session ID from the server
-  if (read(resp_fd, &session_id, sizeof(session_id)) == -1) {
-    perror("Error receiving session ID from the server");
-    return 1;
-  }
-
-  return 0;  // Success
+  return 0;
 }
 
-int ems_quit(void) {
-  // Close pipes and reset the active session
-  close(req_fd);
-  close(resp_fd);
-  unlink(req_pipe_path);
-  unlink(resp_pipe_path);
-
-  // Send session termination request to the server
-  if (write(server_pipe, &session_id, sizeof(session_id)) == -1) {
-    perror("Error sending session termination request to the server");
+int ems_quit() {
+  // Send session end message to server
+  int req_fd = open(sessions[active_sessions - 1].req_pipe_path, O_WRONLY);
+  if (req_fd < 0) {
     return 1;
   }
+  write(req_fd, "QUIT", 5);
 
-  session_id = -1;  // Reset the active session ID
-  return 0;  // Success
+  // Close named pipes
+  close(req_fd);
+  int resp_fd = open(sessions[active_sessions - 1].resp_pipe_path, O_RDONLY);
+  if (resp_fd >= 0) {
+    close(resp_fd);
+  }
+
+  // Delete client named pipe
+  unlink(sessions[active_sessions - 1].req_pipe_path);
+  unlink(sessions[active_sessions - 1].resp_pipe_path);
+  active_sessions--;
+
+  return 0;
 }
 
 int ems_create(unsigned int event_id, size_t num_rows, size_t num_cols) {
-  //TODO: send create request to the server (through the request pipe) and wait for the response (through the response pipe)
-  return 1;
+  // Send create request to server through named pipe
+  int req_fd = open(sessions[active_sessions - 1].req_pipe_path, O_WRONLY);
+  if (req_fd < 0) {
+    return 1;
+  }
+  write(req_fd, &event_id, sizeof(unsigned int));
+  write(req_fd, &num_rows, sizeof(size_t));
+  write(req_fd, &num_cols, sizeof(size_t));
+
+  // Handle server response
+  int resp_fd = open(sessions[active_sessions - 1].resp_pipe_path, O_RDONLY);
+  if (resp_fd < 0) {
+    return 1;
+  }
+  int result;
+  read(resp_fd, &result, sizeof(int));
+
+  close(req_fd);
+  close(resp_fd);
+
+  return result;
 }
 
-int ems_reserve(unsigned int event_id, size_t num_seats, size_t* xs, size_t* ys) {
-  //TODO: send reserve request to the server (through the request pipe) and wait for the response (through the response pipe)
-  return 1;
+int ems_reserve(unsigned int event_id, size_t row, size_t col) {
+  // Send reserve request to server through named pipe
+  int req_fd = open(sessions[active_sessions - 1].req_pipe_path, O_WRONLY);
+  if (req_fd < 0) {
+    return 1;
+  }
+  write(req_fd, &event_id, sizeof(unsigned int));
+  write(req_fd, &row, sizeof(size_t));
+  write(req_fd, &col, sizeof(size_t));
+
+  // Handle server response
+  int resp_fd = open(sessions[active_sessions - 1].resp_pipe_path, O_RDONLY);
+  if (resp_fd < 0) {
+    return 1;
+  }
+  int result;
+  read(resp_fd, &result, sizeof(int));
+
+  close(req_fd);
+  close(resp_fd);
+
+  return result;
 }
 
-int ems_show(int out_fd, unsigned int event_id) {
-  //TODO: send show request to the server (through the request pipe) and wait for the response (through the response pipe)
-  return 1;
+int ems_show(unsigned int event_id) {
+  // Send show request to server through named pipe
+  int req_fd = open(sessions[active_sessions - 1].req_pipe_path, O_WRONLY);
+  if (req_fd < 0) {
+    return 1;
+  }
+  write(req_fd, &event_id, sizeof(unsigned int));
+
+  // Handle server response
+  int resp_fd = open(sessions[active_sessions - 1].resp_pipe_path, O_RDONLY);
+  if (resp_fd < 0) {
+    return 1;
+  }
+  int result;
+  read(resp_fd, &result, sizeof(int));
+
+  close(req_fd);
+  close(resp_fd);
+
+  return result;
 }
 
-int ems_list_events(int out_fd) {
-  //TODO: send list request to the server (through the request pipe) and wait for the response (through the response pipe)
-  return 1;
+int ems_list_events() {
+  // Send list events request to server through named pipe
+  int req_fd = open(sessions[active_sessions - 1].req_pipe_path, O_WRONLY);
+  if (req_fd < 0) {
+    return 1;
+  }
+  write(req_fd, "LIST", 5);
+
+  // Handle server response
+  int resp_fd = open(sessions[active_sessions - 1].resp_pipe_path, O_RDONLY);
+  if (resp_fd < 0) {
+    return 1;
+  }
+  int result;
+  read(resp_fd, &result, sizeof(int));
+
+  close(req_fd);
+  close(resp_fd);
+
+  return result;
 }
